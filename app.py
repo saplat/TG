f38f6bd680d15b0921e6ffa78ce19547da8dca20
saplat <41904572+saplat@users.noreply.github.com>
2021-03-20 15:18:12 +0300

    'wtf'

--- app.py
+++ app.py
@@ -3,6 +3,4 @@ from aiogram import executor
 from loader import dp
 from handlers.users.start import start
 
-
-
 executor.start_polling(dp, skip_updates=True)
--- data/config.py
+++ data/config.py
@@ -5,7 +5,8 @@ load_dotenv()
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 
 admins = [
-    1362443966, 435044879
+    1362443966,
+    435044879
 ]
 
 ip = os.getenv("ip")
