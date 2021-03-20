a8bf968ff61cf5be7f6cb917149f12b38b327d0c
GypsyBaby <2ssdmit@gmail.com>
2021-03-21 00:11:09 +0300

    'hey'

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -3,11 +3,11 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp, bot
 
 
-# @dp.message_handler(CommandStart())
-# async def start(message):
-# await message.ansewer(f'привет {message.from_user.full_name}')
-@dp.message_handler(text="/start")
+@dp.message_handler(CommandStart())
 async def start(message: types.Message):
-    chat_id = message.chat.id
-    text = "Хай ма бой"
-    await bot.send_message(chat_id=chat_id, text=text)
+    await message.answer(f'Хай ма бой, многоуважаемый {message.from_user.full_name}')
+# @dp.message_handler(text="/start")
+# async def start(message: types.Message):
+#    chat_id = message.chat.id
+#    text = "Хай ма бой"
+#    await bot.send_message(chat_id=chat_id, text=text)
