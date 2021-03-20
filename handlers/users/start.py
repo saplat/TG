550a3f900ebfe563853b794baaf03cb08a40b57a
GypsyBaby <2ssdmit@gmail.com>
2021-03-21 00:12:13 +0300

    'hey'

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,8 +6,4 @@ from loader import dp, bot
 @dp.message_handler(CommandStart())
 async def start(message: types.Message):
     await message.answer(f'Хай ма бой, многоуважаемый {message.from_user.full_name}')
-# @dp.message_handler(text="/start")
-# async def start(message: types.Message):
-#    chat_id = message.chat.id
-#    text = "Хай ма бой"
-#    await bot.send_message(chat_id=chat_id, text=text)
+
