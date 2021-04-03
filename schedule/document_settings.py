f304456e895bb35cf9bd8cdd39ad6593dafe3a9d
GypsyBaby <2ssdmit@gmail.com>
2021-04-04 00:36:43 +0300

    'schedule'

---
+++ schedule/document_settings.py
@@ -0,0 +1,17 @@
+from enum import IntEnum
+
+DAY_SPLIT_DATA = "Днинед."
+TIME_SPLIT_DATA = "Часы"
+CLASS_SPLIT_DATA = "№ауд."
+
+HEADER_ROW = 3
+START_PARSE_ROW = 4
+START_PARSE_COL = 1
+ROWS_ON_DAY = 10
+END_PARSE_ROW = START_PARSE_ROW + 6 * ROWS_ON_DAY
+
+class StructParse(IntEnum):
+    Day = 1
+    Time = 2
+    GroupName = 3
+    NumberClass = 4
